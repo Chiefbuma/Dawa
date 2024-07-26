aeacd231ddbc5f86cfe525486f74d89d55a151d9
Chiefbuma <jorgebuma.jb@gmail.com>
2024-07-26 15:39:25 +0300

    ew

--- Billing.py
+++ Billing.py
@@ -178,7 +178,7 @@ def app():
 
             # List of columns to hide
             book_columns = [
-                "BookingDate", "Bookedon", "BookedBy", "Facility",
+                "BookingDate", "Booked on", "Booked By", "Facility",
                 "DoctorName", "TeleDoctor", "BookingComments", "Dispatchedstatus",
                 "DispatchedDate", "DispatchedBy", "DispatchComments", "BillingComments","mobile"
                 "Collectionstatus", "CollectionDate", "Month", "Year","BilledDate","BilledBy","BookedBy","ID"
