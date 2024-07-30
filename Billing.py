85bea25e2bf03d3de6f79a73088fdb0a3bc9c7a4
Chiefbuma <jorgebuma.jb@gmail.com>
2024-07-30 12:15:12 +0300

    ew

--- Billing.py
+++ Billing.py
@@ -345,9 +345,10 @@ def app():
                 selected_row = response['selected_rows']
                 
                 if 'Patient_name' not in st.session_state:
-                    st.session_state.Patient_name = 'George' 
+                    st.session_state.Patient_name = '' 
+                    selected_row=Trans_df
                     
-                if  len(selected_row) > 0:
+                if selected_row and len(selected_row) > 0:
                     st.session_state.Patient_name = selected_row[0]['Patientname']
                     #st.write(Patient_name)
                     #st.write("Selected Row:", selected_row)
