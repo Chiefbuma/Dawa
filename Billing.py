60ca80794a611319ba9470bd027d412820019108
Chiefbuma <jorgebuma.jb@gmail.com>
2024-07-30 12:48:56 +0300

    ew

--- Billing.py
+++ Billing.py
@@ -349,7 +349,7 @@ def app():
                     st.session_state.Patient_name = ''
 
                 
-                if not selected_row:
+                if  selected_row:
                     try:
                         patient_name = selected_row.iloc[0]['Patientname']
                         st.session_state.Patient_name = patient_name
