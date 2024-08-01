24cbf496e2113bf1d9eb203f416711fc2807837c
Chiefbuma <jorgebuma.jb@gmail.com>
2024-08-01 15:23:37 +0300

    ew

--- Receipt.py
+++ Receipt.py
@@ -105,9 +105,8 @@ def app():
             
             Trans_df = AllTrans_df[
                     (AllTrans_df['Dispatched status'] == 'Dispatched') & 
-                    (AllTrans_df['Location'] == location) & 
-                    (AllTrans_df['Received Status']=='')]
-            
+                    (AllTrans_df['Location'] == location) 
+                  
             #st.write(Trans_df)
             
             Trans_df['Received Date'] = Trans_df['Received Date'].fillna(formatted_date)
