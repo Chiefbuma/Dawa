64804716156e940c32db67fdbac66bf58701f498
Chiefbuma <jorgebuma.jb@gmail.com>
2024-08-28 11:26:43 +0300

    ew

--- Transfer.py
+++ Transfer.py
@@ -553,7 +553,7 @@ def app():
                 
                 if selected_option == "Transfer In":
                     
-                    pres_df = df[(df['Received Status'] == 'Received') & (df['Transfer To'] == location)]
+                    pres_df = df['Transfer To'] == location
  
                     pres_df=pres_df[[
                                 "ID",
