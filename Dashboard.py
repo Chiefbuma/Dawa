7bc310a99a2401a23aae93674d7e51c2b2fb7376
Chiefbuma <jorgebuma.jb@gmail.com>
2024-09-17 16:30:35 +0300

    ew

--- Dashboard.py
+++ Dashboard.py
@@ -69,10 +69,8 @@ def app():
                         "MVC"
                 ]
                 
-                sharepoint = SharePoint()
-                
                 try:
-                    clients = sharepoint.get_list_items_paginated(list_name="Home Delivery", row_limit=100)
+                    clients = SharePoint().connect_to_list(ls_name='Home Delivery', columns=columns)
                     df = pd.DataFrame(clients)
                     
                     # Ensure all specified columns are in the DataFrame, even if empty
@@ -89,7 +87,7 @@ def app():
             
             #st.write(cycle_df)
             
- 
+            
             
             # Get a list of unique values in the 'Cycle' column
             Cycle = cycle_df['Cycle'].unique().tolist()
