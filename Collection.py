1811215f0f50c78bd9e28bed31379de82bf13b2c
Chiefbuma <jorgebuma.jb@gmail.com>
2024-09-23 14:37:02 +0300

    rw

--- Collection.py
+++ Collection.py
@@ -70,7 +70,7 @@ def app():
                                 Trans_df = AllTrans_df[
                                         (AllTrans_df['Received Status'] == 'Received') &
                                         (AllTrans_df['Transaction Type']=='Receipt') &
-                                        (AllTrans_df['Collection status'] == "")]
+                                        (AllTrans_df['Collection status'] != "Full")]
                             else:   
                                 Trans_df = AllTrans_df[
                                         (AllTrans_df['Received Status'] == 'Received') & 
