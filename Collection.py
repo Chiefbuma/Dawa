ddbc2f7d3ea08a35435a7a51a40ec0b915846afb
Chiefbuma <jorgebuma.jb@gmail.com>
2024-09-23 15:50:26 +0300

    ew

--- Collection.py
+++ Collection.py
@@ -70,13 +70,13 @@ def app():
                                 Trans_df = AllTrans_df[
                                         (AllTrans_df['Received Status'] == 'Received') &
                                         (AllTrans_df['Transaction Type']=='Receipt') &
-                                        (AllTrans_df['Collection status'] == "NULL")]
+                                        (AllTrans_df['Collection Date'] !="")]
                             else:   
                                 Trans_df = AllTrans_df[
                                         (AllTrans_df['Received Status'] == 'Received') & 
                                         (AllTrans_df['Location'] == location) & 
                                         (AllTrans_df['Transaction Type']=='Receipt') &
-                                        (AllTrans_df['Collection status'] == "NULL")]
+                                        (AllTrans_df['Collection Date'] !="")]
                 
              #st.write(Trans_df)
             current_date = datetime.now().date()
