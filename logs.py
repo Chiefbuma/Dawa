c144451c5efcada27150db3e9a918a619db450d5
Chiefbuma <jorgebuma.jb@gmail.com>
2024-10-07 10:23:05 +0300

    ew

--- logs.py
+++ logs.py
@@ -12,7 +12,6 @@ import streamlit_option_menu as option_menu
 import streamlit_shadcn_ui as ui
 from local_components import card_container
 from streamlit_shadcn_ui import slider, input, textarea, radio_group, switch
-from sharepoint import SharePoint
 from postgrest import APIError
 from IPython.display import HTML
 from streamlit_dynamic_filters import DynamicFilters
