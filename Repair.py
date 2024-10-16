f9bc824e26c03baa3961bd092d196a69155cc515
Chiefbuma <jorgebuma.jb@gmail.com>
2024-10-16 15:53:19 +0300

    w

--- Repair.py
+++ Repair.py
@@ -220,7 +220,14 @@ def app():
                 container = st.container(border=True, height=500)
                 with container:
                     
-                    Overall_df= Main_df[['Clinic','Departmental report','Title','Approver']]
+                    @st.cache_data(ttl=600, max_entries=100, show_spinner=False, persist=False, experimental_allow_widgets=False)
+                    def load_new():
+                            New = SharePonitLsist().connect_to_list(ls_name='Maintenance Report')
+                            return pd.DataFrame(  New )
+                        
+                    df_check=load_new()
+                    
+                    Overall_df= df_check[['Clinic','Departmental report','Title','Approver']]
                     
                     Overall_df['Requests'] = int(Overall_df['Title'].nunique())
                     
@@ -331,9 +338,14 @@ def app():
 
                         
                         st.markdown('<div style="height: 0px; overflow-y: scroll;">', unsafe_allow_html=True)
-                
+                        @st.cache_data(ttl=600, max_entries=100, show_spinner=False, persist=False, experimental_allow_widgets=False)
+                        def load_new():
+                                New = SharePonitLsist().connect_to_list(ls_name='Maintenance Report')
+                                return pd.DataFrame(  New )
+                            
+                        df_main=load_new()
                         
-                        data_df= Main_df[['ID','Date of report','Clinic','Department','Amount on the Quotation','MainStatus','Approver','MonthName','LinkEdit']]
+                        data_df= df_main[['ID','Date of report','Clinic','Department','Amount on the Quotation','MainStatus','Approver','MonthName','LinkEdit']]
                         
                         # Convert 'bill_date' to datetime type
                         data_df['Date of report'] = pd.to_datetime(data_df['Date of report']).dt.date
